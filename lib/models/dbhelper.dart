58d5b3702f161c1c1c5827e2b5000598cb9161ab
mutiadhewanty <mutiadhewanty@gmail.com>
2022-03-29 14:06:11 +0700

    first commit

---
+++ lib/models/dbhelper.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'item.dart';
+
+class DbHelper {
+  late DbHelper _dbHelper;
+  late Database _database;
+  DbHelper._createObject();
+
+  Future<Database> initDb() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'item.db';
+
+    var itemDatabase = openDatabase(path, version: 1, onCreate: _createDb);
+    return itemDatabase;
+  }
+
+  void _createDb(Database db, int version) async{
+    await db.execute(
+      '''
+      create table item(
+        id integer primary key autoincrement,
+        name text,
+        price integer
+        )
+      '''
+    );
+  }
+    //create database
+    Future<int> insert(Item object) async {
+      Database db = await this.database;
+      int count = await db.insert('item', object.toMap());
+      return count;
+    }
+    //update database
+    Future<int> update(Item object) async {
+      Database db = await this.database;
+      int count = await db.update('item', object.toMap(),
+      where: 'id=?',
+      whereArgs: [object.id]);
+      return count;
+    }    
+    //delete database
+    Future<int> delete(Item object) async {
+      Database db = await this.database;
+      int count = await db.delete('item',
+      where: 'id=?',
+      whereArgs: ['id']);
+      return count;
+    }  
+    Future<List<Item>> getItemList() async {
+      var itemMapList = await select();
+      int count = itemMapList.length;
+      List<Item> itemList = List<Item>();
+      for (var i = 0; i < count; i++) {
+        itemList.add(Item.fromMap(itemMapList[i]));
+      }
+      return itemList;
+    }  
+    factory DbHelper(){
+      if (_dbHelper == null) {
+        _dbHelper = DbHelper._createObject();
+      }
+      return _dbHelper;
+    }
+    Future<Database> get database async {
+      if (_database == null) {
+        _database = await initDb();
+      }
+      return _database;
+    }
+}
---
+++ lib/models/item.dart
@@ -0,0 +1,34 @@
+class Item {
+  late int _id;
+  late String _name;
+  late int _price;
+
+  int get id => _id;
+
+  String get name => this._name;
+
+  set name(String value) {
+    this._name = value;
+  }
+
+  int get price => this._price;
+
+  set price(int value) {
+    this._price = value;
+  }
+  Item(this._name, this._price);
+
+  Item.fromMap(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._name = map['name'];
+    this._price = map['price'];
+  }
+  
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = Map<String, dynamic>();
+    map['id'] = this._id;
+    map['name'] = name;
+    map['price'] = price;
+    return map;
+  }
+}
